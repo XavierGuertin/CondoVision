247fe4124c8f5e1a1e5269a1177b8416a713d115
Xavier Guertin <xavierguertin@gmail.com>
2024-02-11 18:09:10 -0500

    CV-101 - Create Landing Page Mobile

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,33 +1,118 @@
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { Button } from "@repo/ui";
+import React from 'react';
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Image,
+    Text,
+    TouchableOpacity,
+} from 'react-native';
 
-export default function Native() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Native</Text>
-      <Button
-        onClick={() => {
-          console.log("Pressed!");
-          alert("Pressed!");
-        }}
-        text="Boop"
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function Example() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.hero}>
+                <Image
+                    style={styles.heroImage}
+                    source={require('./assets/logoBright.png')}
+                    resizeMode="contain"
+                />
+            </View>
+            <View style={styles.content}>
+                <View style={styles.contentHeader}>
+                    <Text style={styles.title}>
+                        Manage Your Condos With{'\n'}with{' '}
+                        <View style={styles.appName}>
+                            <Text style={styles.appNameText}>Condo Vision</Text>
+                        </View>
+                    </Text>
+                    <Text style={styles.text}>
+                        Condo Vision is a comprehensive condo management system designed for the modern era.
+                        Our platform seamlessly integrates the needs of condo owners, rental users, and condo
+                        management companies, offering a streamlined and intuitive experience.
+                    </Text>
+                </View>
+
+                <TouchableOpacity
+                    onPress={() => {
+                        // handle onPress
+                    }}>
+                    <View style={styles.button}>
+                        <Text style={styles.buttonText}>Log In</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    fontWeight: "bold",
-    marginBottom: 20,
-    fontSize: 36,
-  },
+    container: {
+        flex: 1,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '500',
+        color: '#281b52',
+        textAlign: 'center',
+        marginBottom: 12,
+        lineHeight: 40,
+    },
+    text: {
+        fontSize: 15,
+        lineHeight: 24,
+        fontWeight: '400',
+        color: '#9992a7',
+        textAlign: 'center',
+    },
+    /** Hero */
+    hero: {
+        backgroundColor: '#3c4142',
+        margin: 12,
+        borderRadius: 16,
+        padding: 16,
+    },
+        heroImage: {
+        width: '100%',
+        height: 400,
+    },
+    /** Content */
+    content: {
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingVertical: 24,
+        paddingHorizontal: 24,
+    },
+    contentHeader: {
+        paddingHorizontal: 24,
+    },
+    appName: {
+        backgroundColor: '#fff2dd',
+        transform: [
+            {
+                rotate: '-5deg',
+            },
+        ],
+        paddingHorizontal: 6,
+    },
+    appNameText: {
+        fontSize: 28,
+        fontWeight: '700',
+        color: '#281b52',
+    },
+    /** Button */
+    button: {
+        backgroundColor: '#56409e',
+        paddingVertical: 12,
+        paddingHorizontal: 14,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 12,
+    },
+    buttonText: {
+        fontSize: 15,
+        fontWeight: '500',
+        color: '#fff',
+    },
+
 });
