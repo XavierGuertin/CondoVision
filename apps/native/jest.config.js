b590f6aca47568e243d8523dbacbc037088069a0
im-samwong <98343790+im-samwong@users.noreply.github.com>
2024-02-28 14:12:15 -0500

    Removed setup files

--- apps/native/jest.config.js
+++ apps/native/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/../../@testing-library/jest-native/extend-expect'],
   // If you're using module aliases, add them here
   moduleNameMapper: {
     '^@ui/(.*)$': '<rootDir>/../../packages/ui/src/$1',
--- apps/web/jest.config.js
+++ apps/web/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['<rootDir>/../../@testing-library/jest-dom/extend-expect'],
   // If you're using module aliases, add them here
   moduleNameMapper: {
     '^@ui/(.*)$': '<rootDir>/../../packages/ui/src/$1',
