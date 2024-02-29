ca3dd8d6d147df922466c35de289c623991bb514
lo-vil <louis.villemure@gmail.com>
2024-02-28 23:02:42 -0500

    Adding Map and Markers

--- apps/native/screens/OwnerMapScreen.tsx
+++ apps/native/screens/OwnerMapScreen.tsx
@@ -1,46 +1,100 @@
 import React, {useEffect, useState} from 'react';
-import {OwnerMapComponent} from "@ui/OwnerMapComponent";
-import {auth, db} from '../../web/firebase.js';
-import {useAuthState} from "react-firebase-hooks/auth";
+import {MapComponent} from "@ui/MapComponent";
+import {db} from '../../web/firebase.js';
 import {
     View,
     SafeAreaView,
     StyleSheet,
     Text,
+    ActivityIndicator,
 } from 'react-native';
-import {collection, getDocs} from "firebase/firestore";
+import {collection, getDocs, query} from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import firebase from "firebase/compat";
+import firestore = firebase.firestore;
+import { Firestore } from 'firebase/firestore';
 
+export function OwnerMapScreen() {
 
-export default function OwnerMapScreen() {
+    const [state, setState] = useState("");
+    const [ownedUnits, setOwnedUnits] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    const [ownderData, setOwnerData] = useState([]);
-    const [user] = useAuthState(auth);
-    const userDocRef = getDoc(collection(db, "users", ))
+    const userID = async () => {
+        try {
+            const value = await AsyncStorage.getItem("userUID");
+            if (value !== null) {
+                return value;
+            }
+        } catch (error) {}
+    };
+
+    const coordinates = [
+        {
+            latitude: 45.49680028420473,
+            longitude: -73.56879175215543,
+        },
+        {
+            latitude: 45.48038685337935,
+            longitude: -73.57726341610069,
+        }
+    ]
 
     useEffect(() => {
-        const propertiesRef = collection(db, "properties");
-        let unitList = [];
-        getDocs(propertiesRef).then((propertiesQuerySnapshot) => {
-            propertiesQuerySnapshot.forEach(propertyDoc => {
-                let condoUnitRef = propertyDoc.ref.collection("CondoUnits");
+        const fetchData = async () => {
+            const unitList = [];
+            try {
+                const propertiesCollectionRef = collection(db, 'properties');
+                const q = query(propertiesCollectionRef);
+                const propertiesCollectionSnapshot = await getDocs(q);
 
-                getDocs(condoUnitRef.where('owner', '==', user)).then((condoUnitSnapshot => {
+                propertiesCollectionSnapshot.forEach(async propertyDoc => {
+                    const condoUnitsCollectionRef = collection(db, 'properties', propertyDoc.id, 'CondoUnits');
+                    const condoUnitsSnapshot = await getDocs(condoUnitsCollectionRef);
+                    var stopper = true
+                    var propertyData = propertyDoc.data();
+                    condoUnitsSnapshot.forEach(condoUnitDoc => {
+                        var condoData = condoUnitDoc.data();
+                        if (condoData.owner == "QSgWxLSwWVX1G40bzvSo7V0WtvP2" && stopper) {
+                            unitList.push({
+                                userId: "QSgWxLSwWVX1G40bzvSo7V0WtvP2",
+                                propertyId: propertyDoc.id,
+                                coordinates: {
+                                    latitude: propertyData.latitude,
+                                    longitude: propertyData.longitude,
+                                }
+                            });
+                            setOwnedUnits(unitList);
+                            stopper = false;
+                        }
+                    });
+                });
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setTimeout(() => {
+                    setIsLoading(false);
+                }, 200);
+            }
+        };
 
-                }))
-            })
-        })
+        fetchData();
     }, []);
 
-    return(
-    <View style={styles.container}>
-        <OwnerMapComponent />
-    </View>
+    return (
+        <View style={styles.container}>
+            {isLoading ? (
+                <ActivityIndicator size="large" color="#0000ff" /> // Show loading indicator while loading
+            ) : (
+                <MapComponent data={ownedUnits} /> // Render map when not loading
+            )}
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1
     },
     text: {
         fontSize: 25,
