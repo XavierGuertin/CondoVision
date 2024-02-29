2e3166217c8dd6a40eef7a4db7623f3d8fa65dd3
lo-vil <louis.villemure@gmail.com>
2024-02-28 23:07:42 -0500

    changing to home navigator

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -8,7 +8,7 @@ const Stack = createNativeStackNavigator();
 function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="OwnerMap"
+            <Stack.Navigator initialRouteName="Home"
                 screenOptions={{
                     headerShown: false, // This line hides the header globally
                 }}
