a7bf2a6923289da27851671e3385516bb27d46e0
yasminehilout <yasmine.hilout@gmail.com>
2024-03-03 16:34:40 -0500

    Added console logs to debug

--- packages/ui/src/PdfUploadButton.tsx
+++ packages/ui/src/PdfUploadButton.tsx
@@ -12,36 +12,48 @@ interface PdfUploadButtonProps {
 
 const PdfUploadButton: React.FC<PdfUploadButtonProps> = ({ propertyId }) => {
   const uploadPDF = async () => {
+    console.log("Upload PDF function triggered");
     try {
+      console.log("Attempting to pick a document...");
       // Use DocumentPicker.pick for single file selection
       const result: DocumentPickerResponse[] =  await DocumentPicker.pick({
         type: [DocumentPicker.types.pdf],
       });
+      console.log("Document picked:", result[0].name);
 
       // Ensure user is signed in 
       const auth = getAuth();
       const user = auth.currentUser;
       if(!user){
+        console.log("No authenticated user found.");
         Alert.alert("Upload Failed", "No authneticated user dounds.");
         return;
       }
+      console.log("Authenticated user:", user.uid); // Log the user's UID
 
       //TODO: Add verification that user is has Property Management role
 
       const fileUri = result[0].uri;
       const fileName = result[0].name;
+      console.log(`File URI: ${fileUri}, File Name: ${fileName}`);
+
       const filePath = `users/${user.uid}/${fileName}`;
       const fileRef = ref(storage, filePath);
+
+      console.log(`File URI: ${fileUri}, File Name: ${fileName}`);
       const response = await fetch(fileUri);
       const blob = await response.blob();
 
       await uploadBytes(fileRef, blob).then(async (snapshot) => {
+        console.log("File uploaded. Getting download URL...");
         const downloadURL = await getDownloadURL(snapshot.ref);
 
         // Add file reference to Firestore in the propertyFiles collection
+        console.log("Adding file reference to Firestore...");
         const propertyFilesRef = collection(db, "properties", propertyId, "propertyFiles");
         await addDoc(propertyFilesRef, { fileName, downloadURL });
 
+        console.log("Upload Success. PDF has been successfully uploaded and linked.");
         Alert.alert("Upload Success", "PDF has been successfully uploaded and linked.");
       });
     } catch (err) {
@@ -56,7 +68,7 @@ const PdfUploadButton: React.FC<PdfUploadButtonProps> = ({ propertyId }) => {
 
   return (
     <TouchableOpacity onPress={uploadPDF} style={{ padding: 10, backgroundColor: '#007bff' }}>
-      <Text style={{ color: '#ffffff' }}>Upload PDF</Text>
+      <Text style={{ color: '#ffffff' }}>Upload PDF new</Text>
     </TouchableOpacity>
   );
 };
