c2fa7edf28444db332981f4b935953252bc104c5
liamd <liam.daigle@gmail.com>
2024-03-02 20:45:46 -0500

    Added missing React import in App.tsx

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,25 +1,34 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen, LoginScreen, SignupScreen, PropertyManagementScreen } from './screens/index'; // screens
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  HomeScreen,
+  LoginScreen,
+  SignupScreen,
+  PropertyManagementScreen,
+} from "./screens/index"; // screens
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home"
-                screenOptions={{
-                    headerShown: false, // This line hides the header globally
-                }}
-            >
-                <Stack.Screen name="Home" component={HomeScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Signup" component={SignupScreen} />
-                <Stack.Screen name="PropertyManagement" component={PropertyManagementScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false, // This line hides the header globally
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
+        <Stack.Screen
+          name="PropertyManagement"
+          component={PropertyManagementScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
