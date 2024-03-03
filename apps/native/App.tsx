f461801f6a132903480b5cea7bdf4e9f74276b6c
yasminehilout <yasmine.hilout@gmail.com>
2024-03-03 16:36:33 -0500

    import react in App

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen, LoginScreen, SignupScreen, PropertyManagementScreen } from './screens/index'; // screens
-
+import React from 'react';
 
 const Stack = createNativeStackNavigator();
 
