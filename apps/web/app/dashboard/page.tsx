388bd857e1d08077dbc1945d0c2380820faddda0
Gabriel <gabrieldubois.eng@gmail.com>
2024-03-13 14:23:21 -0400

    some ui adjustments, dashboard looks like wireframe from figma

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -4,13 +4,20 @@ import {useAuthState} from "react-firebase-hooks/auth";
 import {auth} from "@/firebase";
 import React from "react";
 import {DashboardNav} from  "@ui/index";
+import DashboardCard from "@ui/dashboard/DashboardCard"
 
 const Page = () => {
     const [authUser] = useAuthState(auth);
 
     return (
-    <div>
+    <div className="h-screen bg-gradient-to-r from-[#87A8FA] to-[#87CCFA] overflow-hidden">
         <DashboardNav/>
+        <div className="grid grid-cols-2 justify-items-center items-center mx-20 h-[90%]">
+            <DashboardCard/>
+            <DashboardCard/>
+            <DashboardCard/>
+            <DashboardCard/>
+        </div>
     </div>
     );
 };
