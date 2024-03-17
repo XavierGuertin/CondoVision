2ecdced47cbd26cc0df3a0529a39e27046c117d0
Adissuu <gabrieldubois.eng@gmail.com>
2024-03-16 23:27:49 -0400

    Created list of properties

--- apps/web/app/propertiesList/page.tsx
+++ apps/web/app/propertiesList/page.tsx
@@ -2,7 +2,7 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/firebase";
 import React from "react";
-import { DashboardNav, DashboardCard } from "@ui/index";
+import { DashboardNav, PropertyList } from "@ui/index";
 
 const Page = () => {
     const [authUser] = useAuthState(auth);
@@ -10,12 +10,8 @@ const Page = () => {
     return (
         <div className="h-screen bg-gradient-to-r from-[#87A8FA] to-[#87CCFA] overflow-hidden">
             <DashboardNav />
-            <div className="grid grid-cols-2 justify-items-center items-center mx-20 h-[90%]">
-                <DashboardCard url="/propertiesList" />
-                <DashboardCard url="/dashboard" />
-                <DashboardCard url="/dashboard" />
-                <DashboardCard url="/dashboard" />
-            </div>
+            <PropertyList user={authUser} />
+            This is properties page
         </div>
     );
 };
