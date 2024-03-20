05ba505d4d82282b54cb3c2e8d31079a8a80e6e6
Adissuu <gabrieldubois.eng@gmail.com>
2024-03-19 23:11:37 -0400

    refactored login page by ordering the imports, applying style, and optimizing conditional rendering statements

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -1,43 +1,52 @@
 'use client';
-import {Footer, SignIn, SignUp, Navbar, Response} from "@ui/index";
-import './auth.css';
-import styles from "@/styles/style";
-import {useAuthState} from "react-firebase-hooks/auth";
-import {auth} from "@/firebase";
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase";
+import { Footer, Navbar, Response, SignIn, SignUp } from "@ui/index";
+import styles from "@/styles/style";
+import "./auth.css";
 
 const Page = () => {
+    // Extracted styles
+    const paddingAndCenterStyles = `${styles.paddingX} ${styles.flexCenter}`;
+
+    // Get authenticated user
     const [authUser] = useAuthState(auth);
 
     return (
         <div className="App bg-primary">
-            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            {/* Navbar */}
+            <div className={paddingAndCenterStyles}>
                 <div className={`${styles.boxWidth} z-[20]`}>
-                    <Navbar/>
+                    <Navbar />
                 </div>
             </div>
-            {/* gradient start */}
-            <div
-                className="absolute z-[0] opacity-45 w-[60%] h-[60%] -bottom-[140%] top-0 -left-[10%] rounded-full blue__gradient"/>
 
+            {/* Gradient background */}
+            <div className="absolute z-[0] opacity-45 w-[60%] h-[60%] -bottom-[140%] top-0 -left-[10%] rounded-full blue__gradient" />
+
+            {/* Sign-in and sign-up sections */}
             <div className="flex justify-between px-16">
-                <SignIn/>
-                <div className="flex flex-col  justify-center items-center">
+                <SignIn />
+                <div className="flex flex-col justify-center items-center">
                     <div className="h-[250px] border-gray-800 border-l w-0"></div>
                     <h1 className="text-white py-4 font-poppins">or</h1>
                     <div className="h-[250px] border-gray-800 border-l w-0"></div>
                 </div>
-                <SignUp/>
+                <SignUp />
             </div>
 
-            <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+            {/* Footer */}
+            <div className={`bg-primary ${paddingAndCenterStyles}`}>
                 <div className={`${styles.boxWidth}`}>
-                    <Footer/>
+                    <Footer />
                 </div>
             </div>
-            {authUser ? <Response success={true} message={"You are logged in"}/> : <></>}
 
+            {/* Display login status */}
+            {authUser && <Response success={true} message={"You are logged in"} />}
         </div>
     );
 };
+
 export default Page;
