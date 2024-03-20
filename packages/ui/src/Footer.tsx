c96a5a4510d24e6c09a8ab7ea4cde2618d2f2e26
Adissuu <gabrieldubois.eng@gmail.com>
2024-03-20 12:58:34 -0400

    separated footer into multiple components to improve readability and maintainablity

--- packages/ui/src/Footer.tsx
+++ packages/ui/src/Footer.tsx
@@ -1,70 +1,74 @@
 'use client';
-import {footerLinks, socialMedia} from "./constants";
+import { footerLinks, socialMedia } from "./constants";
 import styles from "@web/styles/style";
 import Image from "next/image";
 import React from "react";
 
+const Logo = () => (
+    <div className="flex-[1] flex flex-col justify-start mr-10">
+        <Image
+            width={175}
+            height={200}
+            src="/logoBright.png"
+            alt="CondoVision Logo"
+            className="object-contain" />
+    </div>
+);
+
+const FooterLinks = () => (
+    <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+        {footerLinks.map((footerlink) => (
+            <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+                <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+                    {footerlink.title}
+                </h4>
+                <ul className="list-none mt-4">
+                    {footerlink.links.map((link, index) => (
+                        <li
+                            key={link.name}
+                            className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                                }`}>
+                            {link.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        ))}
+    </div>
+);
+
+const SocialMediaLinks = () => (
+    <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+            <Image
+                key={social.id}
+                src={social.icon}
+                alt={social.id}
+                width={21}
+                height={21}
+                className={`object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+                    }`}
+                onClick={() => window.open(social.link)}
+            />
+        ))}
+    </div>
+);
+
 const Footer = () => (
     <section id="contactFooter" className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
         <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-            <div className="flex-[1] flex flex-col justify-start mr-10">
-                <Image
-                    width={175}
-                    height={200}
-                    src="/logoBright.png"
-                    alt="CondoVision Logo"
-                    className="object-contain"
-                />
-            </div>
-
-            <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
-                {footerLinks.map((footerlink) => (
-                    <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
-                        <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
-                            {footerlink.title}
-                        </h4>
-                        <ul className="list-none mt-4">
-                            {footerlink.links.map((link, index) => (
-                                <li
-                                    key={link.name}
-                                    className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${
-                                        index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
-                                    }`}
-                                >
-                                    {link.name}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
-            </div>
+            <Logo />
+            <FooterLinks />
         </div>
-
         <div
             className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
             <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
                 Copyright Ⓒ 2023 CondoVision. All Rights Reserved.
             </p>
-
             <a href="/customerSupport" id="contactUs" className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
                 Contact Us
             </a>
-
-            <div className="flex flex-row md:mt-0 mt-6">
-                {socialMedia.map((social, index) => (
-                    <Image
-                        key={social.id}
-                        src={social.icon}
-                        alt={social.id}
-                        width={21}
-                        height={21}
-                        className={`object-contain cursor-pointer ${
-                            index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
-                        }`}
-                        onClick={() => window.open(social.link)}
-                    />
-                ))}
-            </div>
+            <SocialMediaLinks />
         </div>
     </section>
 );
