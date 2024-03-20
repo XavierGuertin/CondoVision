970eefba27c49e6ffd54f5bcc5f97313d746326a
Adissuu <gabrieldubois.eng@gmail.com>
2024-03-20 15:55:10 -0400

    small refactor by adding a z index positive to force the profile modal to be displayed on top

--- packages/ui/src/UserProfileModal.tsx
+++ packages/ui/src/UserProfileModal.tsx
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { IoClose } from 'react-icons/io5';
 import { TbLogout2 } from "react-icons/tb";
-import {auth, db, storage} from "@web/firebase";
-import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
-import {doc, getDoc, updateDoc} from "firebase/firestore";
+import { auth, db, storage } from "@web/firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import Image from "next/image";
-import {signOut} from "firebase/auth";
-import {SingleImageDropzone} from "./SingleImageDropzone";
+import { signOut } from "firebase/auth";
+import { SingleImageDropzone } from "./SingleImageDropzone";
 import Response from "./Response";
-import {Button} from "./button";
+import { Button } from "./button";
 
 const UserProfileModal = ({ user, onClose }: any) => {
 
@@ -24,14 +24,14 @@ const UserProfileModal = ({ user, onClose }: any) => {
     const [editMode, setEditMode] = useState(false);
     const [file, setFile] = useState<File>();
     const [newProfilePicture, setNewProfilePicture] = useState(false);
-    const [responseMessage, setResponseMessage] = useState({success: false, message: ''});
+    const [responseMessage, setResponseMessage] = useState({ success: false, message: '' });
 
 
     useEffect(() => {
         const fetchUserProfileAndAuthData = async () => {
             const user = auth.currentUser;
             if (user) {
-                const {uid, email, displayName, phoneNumber, photoURL} = user;
+                const { uid, email, displayName, phoneNumber, photoURL } = user;
                 if (photoURL) {
                     setImageUrl(photoURL);
                 } else {
@@ -105,7 +105,7 @@ const UserProfileModal = ({ user, onClose }: any) => {
                             },
                             (error) => {
                                 console.error("Error uploading profile picture: ", error);
-                                setResponseMessage({success: false, message: "Failed to upload profile picture"});
+                                setResponseMessage({ success: false, message: "Failed to upload profile picture" });
 
                             },
                             () => {
@@ -114,10 +114,10 @@ const UserProfileModal = ({ user, onClose }: any) => {
                                     const userRef = doc(db, 'users', user.uid);
                                     updateDoc(userRef, { photoURL: url }).then(() => {
                                         console.log('Profile picture updated successfully');
-                                        setResponseMessage({success: true, message: "Profile picture updated successfully"});
+                                        setResponseMessage({ success: true, message: "Profile picture updated successfully" });
                                     }).catch((error) => {
                                         console.error("Error updating document: ", error);
-                                        setResponseMessage({success: false, message: "Failed to update profile picture"});
+                                        setResponseMessage({ success: false, message: "Failed to update profile picture" });
                                     });
                                 });
                             }
@@ -125,16 +125,16 @@ const UserProfileModal = ({ user, onClose }: any) => {
                     } else {
                         // Handle the case where file is undefined
                         console.error("No file selected for upload.");
-                        setResponseMessage({success: false, message: "No file selected for upload."});
+                        setResponseMessage({ success: false, message: "No file selected for upload." });
                     }
                 }
 
                 await updateDoc(userRef, userProfile);
-                setResponseMessage({success: true, message: "Profile updated successfully"});
+                setResponseMessage({ success: true, message: "Profile updated successfully" });
                 setEditMode(false);
             } catch (error) {
                 console.error("Error updating document: ", error);
-                setResponseMessage({success: false, message: "Failed to update profile"});
+                setResponseMessage({ success: false, message: "Failed to update profile" });
             }
         }
     };
@@ -142,7 +142,7 @@ const UserProfileModal = ({ user, onClose }: any) => {
     const userSignOut = () => {
         signOut(auth)
             .then(() => {
-                setResponseMessage({success: true, message: "Logged out successfully"});
+                setResponseMessage({ success: true, message: "Logged out successfully" });
                 console.log("sign out successful");
                 window.localStorage.setItem('userUID', "");
                 window.localStorage.setItem('userRole', "");
@@ -150,30 +150,29 @@ const UserProfileModal = ({ user, onClose }: any) => {
                 window.location.href = "/";
             })
             .catch((error) => console.log(error));
-            setResponseMessage({success: false, message: "Failed to log out"});
+        setResponseMessage({ success: false, message: "Failed to log out" });
     };
 
     return (
         <div
-            className="fixed right-0 top-10 bottom-10 mt-20 w-1/4 max-sm:w-3/4 xs:w-3/4 sm:w-1/3 md:w-1/4 lg:w-1/5 bg-white p-6 overflow-auto rounded-lg shadow-xl transition-all duration-500 delay-200 transform translate-x-full ease-out transition-medium m-4 border-black border-2"
-            style={{transform: 'translateX(0)', height: 'calc(85%)'}}>
+            className="fixed z-10 right-0 top-10 bottom-10 mt-20 w-1/4 max-sm:w-3/4 xs:w-3/4 sm:w-1/3 md:w-1/4 lg:w-1/5 bg-white p-6 overflow-auto rounded-lg shadow-xl transition-all duration-500 delay-200 transform translate-x-full ease-out transition-medium m-4 border-black border-2"
+            style={{ transform: 'translateX(0)', height: 'calc(85%)' }}>
             <div className="flex justify-between">
                 <button onClick={userSignOut}
-                        className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold w-24 h-10 rounded mr-4">
-                    <TbLogout2 className="text-2xl"/>
+                    className="flex items-center justify-center bg-blue-500 hover:bg-blue-700 text-white font-bold w-24 h-10 rounded mr-4">
+                    <TbLogout2 className="text-2xl" />
                     <span>Log Out</span>
                 </button>
                 <button onClick={onClose}
-                        className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-bold rounded-full w-10 h-10">
-                    <IoClose className="text-2xl"/>
+                    className="flex items-center justify-center bg-red-500 hover:bg-red-700 text-white font-bold rounded-full w-10 h-10">
+                    <IoClose className="text-2xl" />
                 </button>
             </div>
             <div className="flex flex-col items-center">
                 <h2 className="text-3xl font-semibold text-gray-800 mb-4 mt-3">User Profile</h2>
-
                 {
                     editMode ?
-                        <div style={{marginTop: 20}}>
+                        <div style={{ marginTop: 20 }}>
                             <div className="text-center">
                                 <b>Profile Picture</b>
                             </div>
@@ -196,7 +195,7 @@ const UserProfileModal = ({ user, onClose }: any) => {
                             src={imageUrl}
                             width={200}
                             height={200}
-                            style={{borderRadius: 5, marginBottom: 20, aspectRatio: 1}}
+                            style={{ borderRadius: 5, marginBottom: 20, aspectRatio: 1 }}
                         />
                 }
                 <div className="space-y-2">
@@ -204,47 +203,47 @@ const UserProfileModal = ({ user, onClose }: any) => {
                         <>
                             <label><strong>Name: </strong></label>
                             <input type="text" value={userProfile.username}
-                                   onChange={(e) => setUserProfile({...userProfile, username: e.target.value})}
-                                   style={{
-                                       padding: '10px',
-                                       border: '1px solid black',
-                                       marginBottom: '10px',
-                                       width: '100%'
-                                   }}/>
-                            <br/>
+                                onChange={(e) => setUserProfile({ ...userProfile, username: e.target.value })}
+                                style={{
+                                    padding: '10px',
+                                    border: '1px solid black',
+                                    marginBottom: '10px',
+                                    width: '100%'
+                                }} />
+                            <br />
                             <label><strong>Phone Number: </strong></label>
                             <input type="text" value={userProfile.phoneNumber}
-                                   onChange={(e) => setUserProfile({...userProfile, phoneNumber: e.target.value})}
-                                   style={{
-                                       padding: '10px',
-                                       border: '1px solid black',
-                                       marginBottom: '10px',
-                                       width: '100%'
-                                   }}/>
+                                onChange={(e) => setUserProfile({ ...userProfile, phoneNumber: e.target.value })}
+                                style={{
+                                    padding: '10px',
+                                    border: '1px solid black',
+                                    marginBottom: '10px',
+                                    width: '100%'
+                                }} />
                         </>
                     ) : (
                         <>
-                            <p style={{fontSize:'1.2rem'}}>
+                            <p style={{ fontSize: '1.2rem' }}>
                                 <strong>Name: </strong> {userProfile.username}</p>
-                            <p style={{fontSize: '1.2rem'}}>
+                            <p style={{ fontSize: '1.2rem' }}>
                                 <strong>Email: </strong> {userProfile.email}</p>
-                            <p style={{fontSize: '1.2rem'}}>
+                            <p style={{ fontSize: '1.2rem' }}>
                                 <strong>Phone Number: </strong> {userProfile.phoneNumber}</p>
-                            <p style={{fontSize: '1.2rem', marginBottom:20}}>
+                            <p style={{ fontSize: '1.2rem', marginBottom: 20 }}>
                                 <strong>Role: </strong> {userProfile.role}</p>
                         </>
                     )}
                 </div>
                 {editMode ? (
                     <>
-                        <Button onClick={handleUpdate} text={"Save Changes"}/>
+                        <Button onClick={handleUpdate} text={"Save Changes"} />
                         <Button onClick={() => {
                             setEditMode(false);
                             setNewProfilePicture(false);
-                        }} text={"Cancel"}/>
+                        }} text={"Cancel"} />
                     </>
                 ) : (
-                    <Button onClick={() => setEditMode(true)} text={"Edit Profile"}/>
+                    <Button onClick={() => setEditMode(true)} text={"Edit Profile"} />
                 )}
 
                 <Response success={responseMessage.success} message={responseMessage.message} />
