d09d9879f620d85569ad2be514739f9ece6d351b
Fetyali7 <fetyali7@gmail.com>
2024-04-01 00:15:08 -0400

    [86azdbnt5] Created Cypress tests for facilitybooking page

---
+++ apps/web/cypress/e2e/properties/facilityBooking.cy.ts
@@ -0,0 +1,51 @@
+
+describe('Facility Booking Process', () => {
+    beforeEach(() => {
+        cy.visit('/login');
+
+        cy.get('[name=emailSignIn]').type('ali@gmail.com');
+        cy.get('[name=passwordSignIn]').type('12341234');
+        cy.get('[name=submitSignIn]').click();
+    });
+
+    it('logs in and books a facility', () => {
+        cy.visit('/properties');
+
+        // Click on a specific property, replace '1234' with the actual property name/text in your app
+        cy.wait(20000);
+        cy.get('div').contains('1234').click();
+
+        // Click the 'Book Facility' button
+        cy.get('button').contains('Book Facility').click();
+
+        // Interaction with the date picker to select April 2nd, 2024
+        cy.get('.react-datepicker__input-container input').click();
+        // Next month
+        cy.get('.react-datepicker__navigation--next').click();
+        // Day 2
+        cy.get('.react-datepicker__day--002:not(.react-datepicker__day--outside-month)').click();
+
+        //Expects alert and dismisses it
+        cy.on('window:alert', (str) => {
+            if (str === 'Time: 11:00 is selected!') {
+                return true; // returning `true` will dismiss the alert
+            }
+        });
+
+        cy.get('button').contains('11:00').click();
+
+        //Expects alert and dismisses it
+        cy.on('window:alert', (str) => {
+            if (str === 'Booking successful!') {
+                return true;
+            }
+        });
+
+        cy.get('button').filter((index, element) => {
+            // @ts-ignore
+            return element.textContent.trim() === 'Book';
+        }).click();
+
+    });
+});
+
