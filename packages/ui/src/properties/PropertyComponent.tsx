4e9105bbbae3bcd80fed8d268bc129c6b8534c10
Fetyali7 <fetyali7@gmail.com>
2024-03-31 23:55:42 -0400

    [86azdbnt5] Added the button to propertyComponenet that opens the facilitybooking page

--- packages/ui/src/properties/PropertyComponent.tsx
+++ packages/ui/src/properties/PropertyComponent.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import DefaultFinanceCard from "../finance/DefaultFinanceCard";
 
-const PropertyComponent = ({ selectedProperty }: any) => {
+
+const PropertyComponent = ({ selectedProperty,onBookFacilityClick }: any) => {
     return (
         selectedProperty &&
         <div className="flex flex-col  h-full w-full h-full border-l border-blue-500">
             <div className="">
                 <h1 className="text-2xl font-bold text-white py-2 pl-2 capitalize">{selectedProperty.propertyName}</h1>
                 <p className="pl-2">Address: {selectedProperty.address}</p>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                    onClick={onBookFacilityClick}
+                >
+                    Book Facility
+                </button>
             </div>
             <div className="w-full h-full pl-2">
                 <h1 className="text-2xl font-bold text-white py-2">Finance</h1>
@@ -19,3 +26,4 @@ const PropertyComponent = ({ selectedProperty }: any) => {
 };
 
 export default PropertyComponent;
+
